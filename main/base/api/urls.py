cc2ba193a560d6b4ce4cf664d781a5eac81614f8
Esin Shadrach Asukwo <esinshadrachrafe@gmail.com>
2023-01-28 14:15:10 +0100

    Delete main directory

--- main/base/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.getRoutes, name=""),
-    path("rooms/", views.getRooms, name=""),
-    path("rooms/<str:pk>/", views.getRoom, name=""),
-]
- 
